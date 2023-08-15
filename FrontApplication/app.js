e07d84ca3d7d2a5ea7390bb98de9efd8bab85ccb
alale <alale@DESKTOP-8AMKDNT>
2023-08-15 13:45:28 +0330

    JS folders Added

---
+++ FrontApplication/app.js
@@ -0,0 +1,59 @@
+// Establish the socket connection
+var socket = io('http://localhost:15003');
+
+socket.on('connect', function () {
+    console.log("Connected to Socket.IO server");
+});
+
+ socket.on('ReceiveMessage', function (symbols) {
+     symbols.forEach(function (symbol) {
+         var row = document.querySelector('[data-id="' + symbol.symbolISIN + '"]');
+         if (row) {
+             var tds = row.querySelectorAll('td');
+             if (symbol.lastTradedPrice) {
+                 tds[0].textContent = symbol.lastTradedPrice;
+                 tds[0].classList.add("flash");
+             }
+
+             if (symbol.closingPrice) {
+                 tds[1].textContent = symbol.closingPrice;
+                 tds[1].classList.add("flash");
+             }
+
+             // Update other fields similarly
+
+             setTimeout(() => {
+                 tds[0].classList.remove("flash");
+                 tds[1].classList.remove("flash");
+             }, 1000);
+         }
+     });
+     console.log("Received message:", symbols);
+ });
+
+const table = document.querySelector('.symbolsTable');
+
+function getData() {
+    fetch('http://localhost:15006/Symbol/v1/Get')
+        .then((res) => res.json())
+        .then((data) => {
+            const tbody = table.querySelector('#priceTableBody');
+            tbody.innerHTML = '';
+
+            data.forEach(element => {
+                const row = document.createElement('tr');
+                row.setAttribute("data-id", `${element.symbolISIN}`);
+                row.innerHTML = `
+                    <td>${element.lastTradedPrice}</td>
+                    <td>${element.closingPrice}</td>
+                    <td>${element.symbolTitle}</td>
+                `;
+                tbody.appendChild(row);
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+getData();
---
+++ UINodeServerApplication/app.js
@@ -0,0 +1,68 @@
+const amqp = require('amqplib');
+const http = require('http');
+const socketIo = require('socket.io');
+
+// RabbitMQ configuration
+const rabbitMQConfig = {
+  host: 'localhost',
+  port: 5672,
+  username: 'guest',
+  password: 'guest'
+};
+
+// Socket.io server configuration
+const socketIoPort = 15003;
+
+// Create an HTTP server
+const server = http.createServer();
+
+// Initialize the Socket.io server with CORS configuration
+const io = socketIo(server, {
+  cors: {
+    origin: ['http://localhost:10204','http://localhost:10203'], // Adjust this to match your frontend's URL
+    methods: ['GET', 'POST']
+  }
+});
+
+// Socket.io connection handler
+io.on('connection', (socket) => {
+  console.log('Socket.io client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Socket.io client disconnected');
+  });
+});
+
+// Start the HTTP server
+server.listen(socketIoPort, () => {
+  console.log(`Socket.io server is listening on port ${socketIoPort}`);
+
+  // After the HTTP server is started, connect to RabbitMQ
+  connectToRabbitMQ().then(() => {
+    console.log('Connected to RabbitMQ');
+  });
+});
+
+// Create a connection to RabbitMQ and consume messages
+async function connectToRabbitMQ() {
+  const connection = await amqp.connect({
+    hostname: rabbitMQConfig.host,
+    port: rabbitMQConfig.port,
+    username: rabbitMQConfig.username,
+    password: rabbitMQConfig.password
+  });
+
+  const channel = await connection.createChannel();
+
+  // Declare the queue
+  const queueName = 'TseData';
+  await channel.assertQueue(queueName, { durable: true });
+
+  // Consume messages from the queue
+  channel.consume(queueName, (message) => {
+    if (message) {
+      const symbols = JSON.parse(message.content.toString());
+      io.emit('ReceiveMessage', symbols); // Emit to 'ReceiveMessage' event
+    }
+  }, { noAck: true });
+}
